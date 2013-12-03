9ba8af0978b575c135b12176c1e72fa5ca63e639
Austin Matzko <austin@pressedcode.com>
2013-12-02 23:03:02 -0500

    Query for posts in the application controller.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,13 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import scala.slick.driver.MySQLDriver.simple._
+
+import models.Post
+
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(Post.find))
   }
-
 }
